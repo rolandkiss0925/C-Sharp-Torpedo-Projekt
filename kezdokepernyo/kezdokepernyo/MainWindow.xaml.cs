27cf481a88319f5bf4bf0735f6e7f8ea07710d25
Greta <kaszagreta000@gmail.com>
2022-12-21 14:06:19 +0100

    kezdokepernyo-alap

---
+++ kezdokepernyo/kezdokepernyo/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kezdokepernyo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        internal void ShowActivated()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnPlayer1vsPlayer2(object sender, RoutedEventArgs e)
+        {
+            var newWindow = new Player1vsPlayer2();
+            newWindow.Show();
+            Close();
+        }
+    }
+}
---
+++ kezdokepernyo/kezdokepernyo/Player1vsPlayer2.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace kezdokepernyo
+{
+    /// <summary>
+    /// Interaction logic for Player1vsPlayer2.xaml
+    /// </summary>
+    public partial class Player1vsPlayer2 : Window
+    {
+        public Player1vsPlayer2()
+        {
+            InitializeComponent();
+        }
+
+        private void Cancel(object sender, RoutedEventArgs e)
+        {
+            var newWindow = new MainWindow();
+            newWindow.ShowActivated();
+            Close();
+        }
+    }
+}
